8c38d6cd8942df50504632c0b1c63acb14b7938c
Uttam Pradhan <uttampra1@gmail.com>
2024-02-23 14:39:07 +0545

    No more after this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            This is also for trial
+            finslly
           </p>
         </a>
 
