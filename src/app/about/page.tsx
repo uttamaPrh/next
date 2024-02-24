d42fe525f2c0d97c6f8b4764a5840a5854b4ae05
Uttam Pradhan <uttampra1@gmail.com>
2024-02-24 23:08:13 +0545

    dynamica and statis routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "About code",
+  description: "About description",
+};
+
 export default function AboutPage() {
   return <h1>About</h1>;
 }
--- src/app/product/[productID]/page.tsx
+++ src/app/product/[productID]/page.tsx
@@ -1,7 +1,39 @@
-export default function ProductPage({
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productID: string;
+  };
+};
+
+// Function to asynchronously generate metadata
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productID: string };
-}) {
-  return <h1>Product Details {params.productID}</h1>;
+}: Props): Promise<Metadata> => {
+  try {
+    const title = await new Promise<string>((resolve) => {
+      setTimeout(() => {
+        resolve(`${params.productID}`);
+      }, 100);
+    });
+
+    // Returning the metadata object
+    return {
+      title: `Product is ${title}`,
+    };
+  } catch (error) {
+    // Handle any errors
+    console.error("Error generating metadata:", error);
+    throw error;
+  }
+};
+
+// Product page component
+export default function ProductPage({ params }: Props) {
+  return (
+    <div>
+      <h1>Product {params.productID}</h1>
+      <p>Page product!</p>
+    </div>
+  );
 }
